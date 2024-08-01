2f5b40433928092b6d02385a1669135eac120871
RaulCudris <raulcudriz@hotmail.com>
2024-08-01 17:18:57 -0500

    first commit

---
+++ src/main/java/com/makiia/springModuleJwtAuth/SpringModuleJwtAuthApplication.java
@@ -0,0 +1,12 @@
+package com.makiia.springModuleJwtAuth;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModuleJwtAuthApplication.class, args);
+	}
+
+}
